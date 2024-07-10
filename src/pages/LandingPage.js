046c853069bcded63aff9946cdf5a83b6412028f
jeong-gayeon <1bfish106@naver.com>
2024-07-10 16:49:22 +0900

    refactor/componentization

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import ReactTypingEffect from 'react-typing-effect';
-import { Button } from '@chakra-ui/react';
+import {Button, Flex, Text} from '@chakra-ui/react';
 import logo from '../logo/jiwooLanding.png';
-import '../LandingPage.css'; // CSS 파일 import
+import '../LandingPage.css';
+import {EditIcon, LockIcon} from "@chakra-ui/icons"; // CSS 파일 import
 
 const LandingPage = () => {
     const navigate = useNavigate();
@@ -14,11 +15,20 @@ const LandingPage = () => {
 
     return (
         <div className="landing-container">
+            <Flex className="landing-user">
+                <Flex mr="4" align="center" cursor="pointer" _hover={{ color: 'teal.200' }}>
+                    <LockIcon mr="1" />
+                    <Text>로그인</Text>
+                </Flex>
+                <Flex align="center" cursor="pointer" _hover={{ color: 'teal.200' }}>
+                    <EditIcon mr="1" />
+                    <Text>회원가입</Text>
+                </Flex>
+            </Flex>
             <img src={logo} className="landing-logo" alt="JIWOO logo" />
             <ReactTypingEffect
                 text={['지혜로운 도우미 JIWOO를 경험해보세요!']}
                 speed={100}
-                eraseDelay={Infinity}
                 className="landing-typing-effect"
             />
             <Button
