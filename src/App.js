e6880d84a334b6159598d642329979bcedbc9b32
jeong-gayeon <1bfish106@naver.com>
2024-07-10 15:44:57 +0900

    feat/LandingPage

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { ChakraProvider } from '@chakra-ui/react';
 import SimilarServicePage from './pages/SimilarAnalysisPage';
 import Header from '../src/component/Header';
 import BusinessSupportPage from "./pages/BusinessSupportPage";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
@@ -14,6 +15,10 @@ function App() {
           <Route path="SimilarService" element={<SimilarServicePage />} />
             <Route path="BusinessSupport" element={<BusinessSupportPage/>}/>
         </Routes>
+
+          <Routes>
+              <Route path="JIWOO" element={<LandingPage/>}/>
+          </Routes>
       </BrowserRouter>
     </ChakraProvider>
   );
