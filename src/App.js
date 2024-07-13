2106aebd5fd5101d167599d4b401b178391970df
jeong-gayeon <1bfish106@naver.com>
2024-07-13 11:32:29 +0900

    refact/footer ADD

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from '../src/component/Header';
 import LandingPage from "./pages/LandingPage";
 import BusinessSupportPage from "./pages/BusinessSupportPage";
 import MarketResearch from "./pages/MarketResearch";
+import Footer from './component/Footer';
 
 function App() {
     return (
@@ -17,18 +18,21 @@ function App() {
                         <>
                             <Header />
                             <SimilarServicePage />
+                            <Footer /> {/* Footer 추가 */}
                         </>
                     } />
                     <Route path="BusinessSupport" element={
                         <>
                             <Header />
                             <BusinessSupportPage />
+                            <Footer /> {/* Footer 추가 */}
                         </>
                     } />
                     <Route path="MarketResearch" element={
                         <>
                             <Header />
                             <MarketResearch />
+                            <Footer /> {/* Footer 추가 */}
                         </>
                     } />
                 </Routes>
